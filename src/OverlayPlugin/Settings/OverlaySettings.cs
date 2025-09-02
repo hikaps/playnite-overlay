ae9a49484412b0934014f0faca7f115c11f324b7
hikaps <notaname@Sharveshs-MacBook-Pro.local>
2025-09-02 11:55:58 -0700

    chore(release): bump extension version to 0.2.0

---
+++ src/OverlayPlugin/Settings/OverlaySettings.cs
@@ -0,0 +1,35 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PlayniteOverlay;
+
+public class OverlaySettings : ObservableObject
+{
+    private bool useControllerToOpen = true;
+    public bool UseControllerToOpen
+    {
+        get => useControllerToOpen;
+        set => SetProperty(ref useControllerToOpen, value);
+    }
+
+    private string controllerCombo = "Guide"; // Placeholder; later map to explicit buttons
+    public string ControllerCombo
+    {
+        get => controllerCombo;
+        set => SetProperty(ref controllerCombo, value);
+    }
+
+    private bool enableCustomHotkey = false;
+    public bool EnableCustomHotkey
+    {
+        get => enableCustomHotkey;
+        set => SetProperty(ref enableCustomHotkey, value);
+    }
+
+    private string customHotkey = "Ctrl+Alt+O";
+    public string CustomHotkey
+    {
+        get => customHotkey;
+        set => SetProperty(ref customHotkey, value);
+    }
+}
+
