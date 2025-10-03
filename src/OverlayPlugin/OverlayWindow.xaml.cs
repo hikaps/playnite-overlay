e528dfbfedb3d9501bb81c9d390fdf8e0a16917f
hikaps <notaname@Sharveshs-MacBook-Pro.local>
2025-10-02 19:08:48 -0700

    feat: add controller navigation

--- src/OverlayPlugin/OverlayWindow.xaml.cs
+++ src/OverlayPlugin/OverlayWindow.xaml.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using System.Collections.Generic;
+using System.Windows.Threading;
 using PlayniteOverlay.Models;
+using PlayniteOverlay.Input;
 
 namespace PlayniteOverlay;
 
@@ -12,14 +15,19 @@ public partial class OverlayWindow : Window
     private readonly Action onSwitch;
     private readonly Action onExit;
     private readonly List<OverlayItem> items;
+    private readonly bool enableControllerNavigation;
+    private OverlayControllerNavigator? controllerNavigator;
+    private NavigationTarget navigationTarget = NavigationTarget.RecentList;
+    private int selectedIndex = -1;
     private bool isClosing;
 
-    public OverlayWindow(Action onSwitch, Action onExit, string title, IEnumerable<OverlayItem> items)
+    public OverlayWindow(Action onSwitch, Action onExit, string title, IEnumerable<OverlayItem> items, bool enableControllerNavigation)
     {
         InitializeComponent();
         this.onSwitch = onSwitch;
         this.onExit = onExit;
         this.items = new List<OverlayItem>(items);
+        this.enableControllerNavigation = enableControllerNavigation;
 
         TitleText.Text = string.IsNullOrWhiteSpace(title) ? "Playnite Overlay" : title;
 
@@ -35,6 +43,25 @@ public partial class OverlayWindow : Window
 
         RecentList.ItemsSource = this.items;
         RecentList.AddHandler(System.Windows.Controls.Button.ClickEvent, new RoutedEventHandler(OnRecentPlayClick));
+        RecentList.SelectionChanged += (_, __) =>
+        {
+            if (RecentList.SelectedIndex >= 0)
+            {
+                selectedIndex = RecentList.SelectedIndex;
+            }
+        };
+
+        if (this.items.Count > 0)
+        {
+            selectedIndex = 0;
+            navigationTarget = NavigationTarget.RecentList;
+            Dispatcher.BeginInvoke(() => FocusListItem(selectedIndex), DispatcherPriority.Loaded);
+        }
+        else
+        {
+            navigationTarget = NavigationTarget.SwitchButton;
+            Dispatcher.BeginInvoke(FocusSwitchButton, DispatcherPriority.Loaded);
+        }
 
         SwitchBtn.Click += (_, __) =>
         {
@@ -54,6 +81,10 @@ public partial class OverlayWindow : Window
         Loaded += (_, __) =>
         {
             Activate(); Focus(); Keyboard.Focus(this);
+            if (this.enableControllerNavigation)
+            {
+                controllerNavigator = new OverlayControllerNavigator(this);
+            }
             try
             {
                 var anim = new System.Windows.Media.Animation.DoubleAnimation(0, 1, new Duration(TimeSpan.FromMilliseconds(180)))
@@ -63,6 +94,11 @@ public partial class OverlayWindow : Window
                 RootCard.BeginAnimation(UIElement.OpacityProperty, anim);
             }
             catch { }
+        };
+        Closed += (_, __) =>
+        {
+            controllerNavigator?.Dispose();
+            controllerNavigator = null;
         };
         Closing += OnClosingWithFade;
     }
@@ -74,6 +110,152 @@ public partial class OverlayWindow : Window
             item.OnSelect?.Invoke();
             Close();
         }
+    }
+
+    internal void ControllerNavigateUp()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        switch (navigationTarget)
+        {
+            case NavigationTarget.RecentList:
+                if (items.Count == 0)
+                {
+                    FocusSwitchButton();
+                    break;
+                }
+                selectedIndex = selectedIndex <= 0 ? items.Count - 1 : selectedIndex - 1;
+                FocusListItem(selectedIndex);
+                break;
+            case NavigationTarget.SwitchButton:
+                if (items.Count > 0)
+                {
+                    navigationTarget = NavigationTarget.RecentList;
+                    selectedIndex = items.Count - 1;
+                    FocusListItem(selectedIndex);
+                }
+                else
+                {
+                    FocusExitButton();
+                }
+                break;
+            case NavigationTarget.ExitButton:
+                FocusSwitchButton();
+                break;
+        }
+    }
+
+    internal void ControllerNavigateDown()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        switch (navigationTarget)
+        {
+            case NavigationTarget.RecentList:
+                if (items.Count == 0)
+                {
+                    FocusSwitchButton();
+                    break;
+                }
+                if (selectedIndex >= items.Count - 1)
+                {
+                    FocusSwitchButton();
+                }
+                else
+                {
+                    selectedIndex++;
+                    FocusListItem(selectedIndex);
+                }
+                break;
+            case NavigationTarget.SwitchButton:
+                FocusExitButton();
+                break;
+            case NavigationTarget.ExitButton:
+                if (items.Count > 0)
+                {
+                    navigationTarget = NavigationTarget.RecentList;
+                    selectedIndex = 0;
+                    FocusListItem(selectedIndex);
+                }
+                else
+                {
+                    FocusSwitchButton();
+                }
+                break;
+        }
+    }
+
+    internal void ControllerNavigateLeft()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        if (navigationTarget != NavigationTarget.RecentList && items.Count > 0)
+        {
+            navigationTarget = NavigationTarget.RecentList;
+            if (selectedIndex < 0)
+            {
+                selectedIndex = 0;
+            }
+            FocusListItem(selectedIndex);
+        }
+    }
+
+    internal void ControllerNavigateRight()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        if (navigationTarget == NavigationTarget.RecentList)
+        {
+            FocusSwitchButton();
+        }
+    }
+
+    internal void ControllerAccept()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        switch (navigationTarget)
+        {
+            case NavigationTarget.RecentList:
+                if (selectedIndex >= 0 && selectedIndex < items.Count)
+                {
+                    var item = items[selectedIndex];
+                    item.OnSelect?.Invoke();
+                    Close();
+                }
+                break;
+            case NavigationTarget.SwitchButton:
+                SwitchBtn.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+                break;
+            case NavigationTarget.ExitButton:
+                ExitBtn.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+                break;
+        }
+    }
+
+    internal void ControllerCancel()
+    {
+        if (!enableControllerNavigation)
+        {
+            return;
+        }
+
+        Close();
     }
 
     private void OnClosingWithFade(object? sender, System.ComponentModel.CancelEventArgs e)
@@ -105,5 +287,85 @@ public partial class OverlayWindow : Window
             this.Closing -= OnClosingWithFade;
             this.Close();
         }
+    }
+
+    private void FocusListItem(int index)
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(() => FocusListItem(index));
+            return;
+        }
+
+        if (items.Count == 0)
+        {
+            FocusSwitchButton();
+            return;
+        }
+
+        if (index < 0)
+        {
+            index = 0;
+        }
+        else if (index >= items.Count)
+        {
+            index = items.Count - 1;
+        }
+
+        navigationTarget = NavigationTarget.RecentList;
+        selectedIndex = index;
+        RecentList.SelectedIndex = selectedIndex;
+
+        if (!TryFocusListContainer())
+        {
+            Dispatcher.BeginInvoke((Action)(() => TryFocusListContainer()), DispatcherPriority.Loaded);
+        }
+    }
+
+    private bool TryFocusListContainer()
+    {
+        var container = RecentList.ItemContainerGenerator.ContainerFromIndex(selectedIndex) as ListBoxItem;
+        if (container == null)
+        {
+            return false;
+        }
+
+        container.Focus();
+        return true;
+    }
+
+    private void FocusSwitchButton()
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(FocusSwitchButton);
+            return;
+        }
+
+        navigationTarget = NavigationTarget.SwitchButton;
+        if (items.Count > 0 && selectedIndex < 0)
+        {
+            selectedIndex = 0;
+        }
+        SwitchBtn.Focus();
+    }
+
+    private void FocusExitButton()
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(FocusExitButton);
+            return;
+        }
+
+        navigationTarget = NavigationTarget.ExitButton;
+        ExitBtn.Focus();
+    }
+
+    private enum NavigationTarget
+    {
+        RecentList,
+        SwitchButton,
+        ExitButton
     }
 }
